b4ce1cd05869ca534e8e33f9192f71dbd9a5fa30
Olha Tymoshchuk <o.tymoshchuk.eng@gmail.com>
2020-06-30 16:15:52 -0400

    app without a styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    
     def show
-        
-        @article = Article.find(params[:id])
+       
     end
 
     def index
@@ -18,7 +19,7 @@ class ArticlesController < ApplicationController
 
     def create
         #@article is an instance variable
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         #render plain: @article.inspect
         if @article.save
             #common key is :notice and :alert
@@ -30,12 +31,27 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfully"
-            redirect_to(@article)
+            redirect_to(@article) 
         else
             render 'edit'
         end
+    end
+
+    def destroy
+       
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private 
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 end
