441876c7619291b764001001b33d9ebd68f5fc03
Olha Tymoshchuk <o.tymoshchuk.eng@gmail.com>
2020-07-13 20:22:45 -0400

    integration test

---
+++ test/integration/create_category_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class CreateCategoryTest < ActionDispatch::IntegrationTest
+  test "get new category form and create category" do
+    #get new categories path
+    get "/categories/new"
+    assert_response :success
+    assert_difference 'Category.count', 1 do
+      post categories_path, params: {category: {name: "Sports"}}
+      assert_response :redirect
+    end
+    #helper method to follow the redirect
+    follow_redirect!
+    #that is was successful
+    assert_response :success
+    assert_match "Sports", response.body
+  end
+
+
+  test "get new category form and reject invalid category submition" do
+    #get new categories path
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category: {name: " "}}
+    end
+    assert_match "errors", response.body
+    #assertions
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+  end
+end
