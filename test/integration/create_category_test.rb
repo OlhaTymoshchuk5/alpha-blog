652415ac7af808f34c94b3dc946cd5493c95680f
Olha Tymoshchuk <o.tymoshchuk.eng@gmail.com>
2020-07-20 09:13:07 -0400

    categories page

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin_user = User.create(username: "olhatym", email: "olha@gmailc.om",
+                              password: "password", admin: true)
+    sign_in_as(@admin_user)    
+  end
+
   test "get new category form and create category" do
     #get new categories path
     get "/categories/new"
